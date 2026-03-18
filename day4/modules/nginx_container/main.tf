e4e3eac548a379970b4b6521f73a4bddc5251abb
MihirUser <mihir@Harini-Vijay-Laptop.localdomain>
2026-03-18 05:26:54 +0000

    Fix: Applied terraform fmt for CI compliance

--- day4/modules/nginx_container/main.tf
+++ day4/modules/nginx_container/main.tf
@@ -14,7 +14,7 @@ resource "docker_image" "nginx" {
 resource "docker_container" "nginx" {
   name  = var.container_name
   image = docker_image.nginx.image_id
-  
+
   ports {
     internal = 80
     external = var.container_port
